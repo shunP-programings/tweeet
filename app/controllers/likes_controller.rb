3de0c792b18640c3b0d6bb34cff9b03b65bf6ec4
Keiichi Takahashi <keiichi.takahashi@gmail.com>
2019-10-24 17:12:15 +0900

    modify tweet model

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,18 +2,14 @@ class LikesController < ApplicationController
   def create
     tweet = Tweet.find(params[:tweet_id])
     user = User.find_by(uid: session[:login_uid])
-    unless tweet.liked?(user)
-      tweet.like(user)
-    end
+    user.like_tweets << tweet
     redirect_to root_path
   end
 
   def destroy
     tweet = Tweet.find(params[:id])
     user = User.find_by(uid: session[:login_uid])
-    if tweet.liked?(user)
-      tweet.unlike(user)
-    end
+    tweet.likes.find_by(user_id: user.id).destroy
     redirect_to root_path
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,16 +4,4 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :like_users, through: :likes, source: :user
-
-  def like(user)
-    likes.create(user_id: user.id)
-  end
-
-  def unlike(user)
-    likes.find_by(user_id: user.id).destroy
-  end
-
-  def liked?(user)
-    like_users.include?(user)
-  end
 end
